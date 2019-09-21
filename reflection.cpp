2fe8a1b44145c42040f4b2ffc55724ca7d408ea2
kuantumleap123 <k3v_w1l@yahoo.com>
2019-09-21 11:24:31 +0700

    Add files via upload

---
+++ reflection.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int width;
+    int height;
+    cout << "Please enter width: ";
+    cin >> width;
+    cout << "Please enter height: ";
+    cin >> height;
+    return 0;
+}
